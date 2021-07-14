a6f88de31e52ea5476c20005a17af02e84d14614
lesterino <lesterleongjr@gmail.com>
2021-07-14 14:00:32 -0700

    starts sidebar & navbar integration

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
@@ -36,6 +36,7 @@ app.use('/places', placesRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
